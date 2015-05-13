dd0d04ea952fb247dbcbfcd3448ea20d061b078a
bhek <bhegarty37@gmail.com>
2015-05-13 21:37:55 +0100

    Loads & displays images

--- Sign Detector/main.cpp
+++ Sign Detector/main.cpp
@@ -4,8 +4,65 @@
 #include "stdafx.h"
 #include "Utilities.h"
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	return 0;
+Mat * sampleImages, *testImages;
+int numberOfSampleImages, numberOfTestImages;
+
+Mat * loadImages(int size, char * fileLocation, char ** files) {
+	Mat * images = new Mat[size];
+
+	for (int i = 0; i < size; i++) {
+		string filename(fileLocation);
+		filename.append(files[i]);
+		images[i] = imread(filename, -1);
+		if (images[i].empty())
+		{
+			cout << "Could not open " << images[i] << endl;
+			return false;
+		}
+	}
+	return images;
 }
 
+int _tmain(int argc, _TCHAR* argv[]) {
+	// Load sample sign images
+	char* sampleFileLocation = "Media/RoadSignRecognitionKnownSigns/";	// Location of sample sign images
+	char* sampleFiles[] = {	// Sample sign images
+		"RoadSignGoLeft.JPG",
+		"RoadSignGoRight.JPG",
+		"RoadSignGoStraight.JPG",
+		"RoadSignNoLeftTurn.JPG",
+		"RoadSignNoParking.JPG",
+		"RoadSignNoRightTurn.JPG",
+		"RoadSignNoStraight.JPG",
+		"RoadSignParking.JPG",
+		"RoadSignYield.JPG"
+	};
+
+	int numberOfSampleImages = sizeof(sampleFiles) / sizeof(sampleFiles[0]);	// Number of sample sign images
+	sampleImages = loadImages(numberOfSampleImages, sampleFileLocation, sampleFiles);
+
+	// Load images to be tested
+	char* testFileLocation = "Media/RoadSignRecognitionUnknownSigns/";	// Location of testing images
+	char* testFiles[] = {	// Test images
+		"RoadSigns1.jpg",
+		"RoadSigns2.jpg",
+		"RoadSigns3.jpg",
+		"RoadSignsComposite1.JPG",
+		"RoadSignsComposite2.JPG"
+	};
+
+	numberOfTestImages = sizeof(testFiles) / sizeof(testFiles[0]);	// Number of images to be tested
+	testImages = loadImages(numberOfTestImages, testFileLocation, testFiles);
+
+	for (int i = 0; i < numberOfSampleImages; i++) {
+		imshow("Sample Images", sampleImages[i]);
+		waitKey(0);
+	}
+
+	for (int i = 0; i < numberOfTestImages; i++) {
+		imshow("Test Images", testImages[i]);
+		waitKey(0);
+	}
+
+	return 0;
+}
